6da525edcf5e60b104982570e6936a89f4b85406
Andy Goldstein <andy.goldstein@gmail.com>
2018-02-06 10:04:09 -0500

    Initial commit
    Signed-off-by: Andy Goldstein <andy.goldstein@gmail.com>

---
+++ vars/andyRetry.groovy
@@ -0,0 +1,15 @@
+def call { retry, cmd ->
+  def rc=0
+
+  for(int i = 1; i <= retry; i++) {
+    rc = sh(script: cmd, returnStatus: true)
+    if(rc == 0) {
+      break
+    }
+    echo "Attempt ${i} of ${retry} failed. Retrying"
+  }
+
+  if(rc != 0) {
+    currentBuild.result = 'FAILED'
+  }
+}
